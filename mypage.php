e34cd6a71bb50a4d3bae1fb14c08cf35d364a811
kekke <koukeishou12@gmail.com>
2024-01-24 10:58:49 +0900

    時間組込

--- mypage.php
+++ mypage.php
@@ -48,6 +48,26 @@ loginCheck();
    <a href="contactform.php" class="menu-item lightblue"> <i class="fa fa-diamond"></i> お問い合わせ</a>
 </nav>
 
+<span id="view_today"></span>
+
+<script type="text/javascript">
+document.getElementById("view_today").innerHTML = getToday();
+
+function getToday() {
+	var now = new Date();
+	var year = now.getFullYear();
+	var mon = now.getMonth()+1; //１足す
+	var day = now.getDate();
+	var you = now.getDay(); //曜日(0～6=日～土)
+
+	//曜日の選択肢
+	var youbi = new Array("日","月","火","水","木","金","土");
+	//出力用
+	var s = year + "年" + mon + "月" + day + "日 (" + youbi[you] + ")";
+	return s;
+}
+</script>
+
     
 </body>
 </html>
--- select.php
+++ select.php
@@ -60,6 +60,26 @@ loginCheck();
   setInterval(changeImage, 3000);
 </script> -->
 
+<span id="view_today"></span>
+
+<script type="text/javascript">
+document.getElementById("view_today").innerHTML = getToday();
+
+function getToday() {
+	var now = new Date();
+	var year = now.getFullYear();
+	var mon = now.getMonth()+1; //１を足すこと
+	var day = now.getDate();
+	var you = now.getDay(); //曜日(0～6=日～土)
+
+	//曜日の選択肢
+	var youbi = new Array("日","月","火","水","木","金","土");
+	//出力用
+	var s = year + "年" + mon + "月" + day + "日 (" + youbi[you] + ")";
+	return s;
+}
+</script>
+
 
 </body>
 </html>
